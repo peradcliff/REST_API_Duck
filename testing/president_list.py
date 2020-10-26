f9a0b2e5f017f8ee1fccc7290be57dd4484530f3
Parker <parkerradcliff@gmail.com>
2020-10-26 11:10:44 -0400

    DuckDuckGo API testing

---
+++ testing/president_list.py
@@ -0,0 +1,55 @@
+import requests
+
+
+def list_of_presidents():
+    return [
+        "Washington",
+        "Adams",
+        "Jefferson",
+        "Madison",
+        "Monroe",
+        "Adams",
+        "Jackson",
+        "Buren",
+        "Harrison",
+        "Tyler",
+        "Polk",
+        "Taylor",
+        "Fillmore",
+        "Pierce",
+        "Buchanan",
+        "Lincoln",
+        "Johnson",
+        "Grant",
+        "Hayes",
+        "Garfield",
+        "Arthur",
+        "Cleveland",
+        "Harrison",
+        "Cleveland",
+        "McKinley",
+        "Roosevelt",
+        "Taft",
+        "Wilson",
+        "Harding",
+        "Coolidge",
+        "Hoover",
+        "Roosevelt",
+        "Truman",
+        "Eisenhower",
+        "Kennedy",
+        "Johnson",
+        "Nixon",
+        "Ford",
+        "Carter",
+        "Reagan",
+        "Bush",
+        "Clinton",
+        "Bush",
+        "Obama",
+        "Trump"
+    ]
+
+
+def main():
+    print("Lab 10")
---
+++ testing/pytest_list/pytest_list.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+
+from testing.president_list import list_of_presidents
+
+
+url_ddg = "https://api.duckduckgo.com"
+
+
+def test_president_list():
+    resp = requests.get(url_ddg + "/?q=presidents+of+the+united+states&format=json")
+    rsp_data = resp.json()
+    president_list = list_of_presidents()
+    is_present = False
+
+    for p in president_list:
+        for last_name in rsp_data["RelatedTopics"]:
+            if p in last_name["Text"]:
+                is_present = True
+
+    assert is_present
